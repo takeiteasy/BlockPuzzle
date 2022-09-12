e7a47e673cecbd6c8793f1167bb87937295c07b5
George Watson <gigolo@hotmail.co.uk>
2022-09-12 12:14:09 +0100

    Initial Commit

---
+++ block-puzzle macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  block-puzzle macOS
+//
+//  Created by George Watson on 12/09/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ block-puzzle macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  block-puzzle macOS
+//
+//  Created by George Watson on 12/09/2022.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
